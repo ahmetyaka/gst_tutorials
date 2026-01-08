86fb345e0f860a82c585020d747c9357f0354d0a
ahmetyaka <yakaahmet23@gmail.com>
2026-01-08 14:34:16 +0300

    gst-basic 1-2

--- main.cpp
+++ main.cpp
@@ -1,131 +1,140 @@
-#include "glib-object.h"
-#include <glib.h>
 #include <gst/gst.h>
+#include <glib.h>
+
+typedef struct _CustomData {
+  GstElement *pipeline;
+  GstElement *video_queue;
+  GstElement *video_convert;
+  GstElement *video_sink;
+} CustomData;
 
 static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data) {
   GMainLoop *loop = (GMainLoop *)data;
 
   switch (GST_MESSAGE_TYPE(msg)) {
-
   case GST_MESSAGE_EOS:
-    g_print("End of stream\n");
+    g_print("Yayın bitti.\n");
     g_main_loop_quit(loop);
     break;
 
   case GST_MESSAGE_ERROR: {
     gchar *debug;
     GError *error;
-
     gst_message_parse_error(msg, &error, &debug);
     g_free(debug);
-
-    g_printerr("Error: %s\n", error->message);
+    g_printerr("Hata: %s\n", error->message);
     g_error_free(error);
-
     g_main_loop_quit(loop);
     break;
   }
   default:
     break;
   }
-
   return TRUE;
 }
 
 static void on_pad_added(GstElement *element, GstPad *pad, gpointer data) {
-  GstPad *sinkpad;
-  GstElement *decoder = (GstElement *)data;
+  CustomData *cdata = (CustomData *)data;
+  GstPad *sinkpad = NULL;
+  GstCaps *caps;
+  GstStructure *str;
+  const gchar *name;
 
-  /* We can now link this pad with the vorbis-decoder sink pad */
-  g_print("Dynamic pad created, linking demuxer/decoder\n");
+  caps = gst_pad_get_current_caps(pad);
+  if (!caps) {
+    caps = gst_pad_query_caps(pad, NULL);
+  }
+  
+  str = gst_caps_get_structure(caps, 0);
+  name = gst_structure_get_name(str);
+  g_print("Pad bulundu: %s\n", name);
 
-  sinkpad = gst_element_get_static_pad(decoder, "sink");
+  if (g_str_has_prefix(name, "video/x-raw")) {
+    sinkpad = gst_element_get_static_pad(cdata->video_queue, "sink");
+  }
 
-  gst_pad_link(pad, sinkpad);
-
-  gst_object_unref(sinkpad);
+  if (sinkpad) {
+    if (!gst_pad_is_linked(sinkpad)) {
+      if (gst_pad_link(pad, sinkpad) == GST_PAD_LINK_OK) {
+          g_print("+++ BAĞLANDI: %s\n", name);
+      } else {
+          g_print("!!! BAĞLANTI HATASI: %s\n", name);
+      }
+    } else {
+        g_print("... Zaten bağlı: %s\n", name);
+    }
+    gst_object_unref(sinkpad);
+  }
+  
+  if (caps) gst_caps_unref(caps);
 }
 
-
-
-int main(int argc, char *argv[]) {
+int tutorial_main(int argc, char *argv[], void *user_data) {
+  CustomData cdata; 
   GMainLoop *loop;
-
-  GstElement *pipeline, *source, *demuxer, *decoder, *conv, *sink, *conv_a, *sink_a;
+  GstElement *source, *decoder;
   GstBus *bus;
   guint bus_watch_id;
 
-  /* Initialisation */
+  // GStreamer'ı başlat
   gst_init(&argc, &argv);
 
   loop = g_main_loop_new(NULL, FALSE);
 
-  /* Check input arguments */
-  // if (argc != 2) {
-  //   g_printerr("Usage: %s <Ogg/Vorbis filename>\n", argv[0]);
-  //   return -1;
-  // }
-
-  /* Create gstreamer elements */
-  pipeline = gst_pipeline_new("video-player");
+  cdata.pipeline = gst_pipeline_new("video-player");
   source = gst_element_factory_make("filesrc", "file-source");
-  decoder = gst_element_factory_make("decodebin3", "decoder");
-  conv = gst_element_factory_make("videoconvert", "converter");
-  sink = gst_element_factory_make("autovideosink", "outputvideo");
-  conv_a = gst_element_factory_make("audioconvert", "audioconverter");
-  sink_a = gst_element_factory_make("autoaudiosink", "outputaudio");
+  
+  // DÜZELTME 1: decodebin3 YERİNE decodebin KULLANIYORUZ
+  decoder = gst_element_factory_make("decodebin", "decoder");
 
-  if (!pipeline || !source || !decoder || !conv || !sink) {
-    g_printerr("One element could not be created. Exiting.\n");
-    return -1;
-  }
+  cdata.video_queue = gst_element_factory_make("queue", "video_queue");
 
-  /* Set up the pipeline */
+  cdata.video_convert = gst_element_factory_make("videoconvert", "video_converter");
+  
+  // macOS için Video Sink
+  cdata.video_sink = gst_element_factory_make("osxvideosink", "outputvideo");
+  if (!cdata.video_sink) cdata.video_sink = gst_element_factory_make("autovideosink", "outputvideo");
+  
+  // DÜZELTME 2: Ses gelmese bile videonun başlaması için senkronizasyonu gevşetiyoruz
+  // Eğer ses akışı bozuksa video onu beklemesin.
 
-  /* we set the input filename to the source element */
+
   g_object_set(G_OBJECT(source), "location", "./bbb.mp4", NULL);
 
-  /* we add a message handler */
-  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+  bus = gst_pipeline_get_bus(GST_PIPELINE(cdata.pipeline));
   bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
   gst_object_unref(bus);
 
-  /* we add all elements into the pipeline */
-  /* file-source | ogg-demuxer | vorbis-decoder | converter | alsa-output */
-  gst_bin_add_many(GST_BIN(pipeline), source, decoder, conv, sink,conv_a,sink_a,
-                   NULL);
+  gst_bin_add_many(GST_BIN(cdata.pipeline), source, decoder, 
+                   cdata.video_queue, cdata.video_convert, cdata.video_sink, 
+                    NULL);
 
-  /* we link the elements together */
-  /* file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output */
-  gst_element_link(source, decoder);
-  gst_element_link(conv, sink);
-  gst_element_link(conv_a, sink_a);
-  g_signal_connect(decoder, "pad-added", G_CALLBACK(on_pad_added), conv_a);
-  g_signal_connect(decoder, "pad-added", G_CALLBACK(on_pad_added), conv);
+  // Statik bağlantılar
+  if (!gst_element_link_many(cdata.video_queue, cdata.video_convert, cdata.video_sink, NULL)) {
+      g_printerr("Video zinciri bağlanamadı.\n");
+  }
+  
+  if (!gst_element_link(source, decoder)) {
+      g_printerr("Source -> Decoder bağlanamadı.\n");
+  }
 
-  /* note that the demuxer will be linked to the decoder dynamically.
-     The reason is that Ogg may contain various streams (for example
-     audio and video). The source pad(s) will be created at run time,
-     by the demuxer when it detects the amount and nature of streams.
-     Therefore we connect a callback function which will be executed
-     when the "pad-added" is emitted.*/
+  // Dinamik bağlantı
+  g_signal_connect(decoder, "pad-added", G_CALLBACK(on_pad_added), &cdata);
 
-  /* Set the pipeline to "playing" state*/
-  g_print("Now playing: %s\n", argv[1]);
-  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  g_print("Pipeline durumu PLAYING yapılıyor...\n");
+  gst_element_set_state(cdata.pipeline, GST_STATE_PLAYING);
 
-  /* Iterate */
-  g_print("Running...\n");
+  g_print("Main Loop Başlıyor...\n");
   g_main_loop_run(loop);
 
-  /* Out of the main loop, clean up nicely */
-  g_print("Returned, stopping playback\n");
-  gst_element_set_state(pipeline, GST_STATE_NULL);
-
-  g_print("Deleting pipeline\n");
-  gst_object_unref(GST_OBJECT(pipeline));
+  gst_element_set_state(cdata.pipeline, GST_STATE_NULL);
+  gst_object_unref(GST_OBJECT(cdata.pipeline));
   g_source_remove(bus_watch_id);
   g_main_loop_unref(loop);
 
   return 0;
+}
+
+int main(int argc, char *argv[]) {
+  return gst_macos_main(tutorial_main, argc, argv, NULL);
 }
